c23948326e20353c686d83f7cd86c626a640e27c
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-06 21:35:34 -0300

    Utilizando 'mongosee-unique-validator' para enviar un mensaje personalizado al encontrar una cláve única repetida. Se utiliza la propiedad 'enum' a la hora de cargar un role para indicar los valores permitidos.

--- models/usuario.js
+++ models/usuario.js
@@ -1,8 +1,15 @@
 // Importación requerida para MongoDB
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 // Definición del Schema
 var Schema = mongoose.Schema;
+
+// Roles válidos
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+};
 
 // Modelo del usuario en la base de datos
 var usuarioSchema = new Schema({
@@ -11,8 +18,12 @@ var usuarioSchema = new Schema({
     email: { type: String, unique: true, required: [true, 'El correo electrónico es requerido'] },
     password: { type: String, required: [true, 'La contraseña es obligatoria'] },
     img: { type: String, required: false },
-    role: { type: String, required: true, default: 'USER_ROLE' }
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
 
 });
+
+// En caso de tener varios 'unique' se puede utilizar la clave en el mensaje.
+// usuarioSchema.plugin(uniqueValidator, { message: 'El {PATH} debe ser único' });
+usuarioSchema.plugin(uniqueValidator, { message: 'El correo electrónico ya existe' });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);

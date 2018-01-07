d3d701ff95b48f1d07b66ca647ed7abd452bc8f9
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-06 21:23:01 -0300

    Cargar un nuevo usuario a mongodb desde datos POST usando bodyParser

--- routes/usuario.js
+++ routes/usuario.js
@@ -7,7 +7,9 @@ var app = express();
 // Obtengo el modelo del usuario
 var Usuario = require('../models/usuario');
 
-// Rutas
+// ==================================================
+// Obtener listado de usuarios
+// ==================================================
 app.get('/', (req, res, next) => {
     Usuario.find({}, 'nombre email img role').exec((err, usuarios) => {
         if (err) {
@@ -21,6 +23,36 @@ app.get('/', (req, res, next) => {
         res.status(200).json({
             error: false,
             usuarios
+        });
+    });
+});
+
+// ==================================================
+// Crear un nuevo usuario
+// ==================================================
+app.post('/', (req, res) => {
+    var body = req.body; // Solo funciona si bodyParser existe
+
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        img: body.img,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioGuardado) => {
+        if (err) {
+            return res.status(500).json({
+                error: true,
+                mensaje: 'Error al tratar de crear el usuario',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            error: false,
+            usuario: usuarioGuardado
         });
     });
 });

2f4421b73111be1c16885659ef48de26f03b2734
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-08 00:00:13 -0300

    Implementación de 'google-auth-library', creación de constantes GOOGLE_CLIENT_ID y GOOGLE_SECRET

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-codigo-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '19929206222-umukesut03ecaqca8sn4lk83cmtfb06s.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'h90e3ntcUL9ExBGbctyp-Np1';
